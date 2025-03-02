1055549ab76c3810650587184e9568649c7fa9f7
Philipp Wachowitz <philipp.wachowitz@gmx.de>
2025-03-02 17:46:56 +0100

    fixed paths again

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // register serviceworker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../sw.js').then(() => {
+    navigator.serviceWorker.register('sw.js').then(() => {
       console.log("Service Worker registered!");
     }).catch((error) => {
       console.error("Service Worker registration failed:", error);
